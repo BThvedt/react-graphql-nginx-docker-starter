f64b113add265c8ffe2cbd758c218711f00424c0
BThvedt <bthvedt@gmail.com>
2020-03-30 11:52:26 -0500

    Update lightsail-startup-script.sh

--- lightsail-startup-script.sh
+++ lightsail-startup-script.sh
@@ -1,5 +1,4 @@
-curl -o ./lightsail.sh
-https://raw.githubusercontent.com/BThvedt/react-graphql-nginx-docker-starter/master/lightsail-compose.sh
+curl -o ./lightsail.sh https://raw.githubusercontent.com/BThvedt/react-graphql-nginx-docker-starter/master/lightsail-compose.sh
 
 chmod +x ./lightsail.sh 
 
